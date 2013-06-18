9f7f73c0727ab473d7417905d9b988f8d6f532a2
Courtney Wallace <crwallace39@gmail.com>
2013-06-18 16:02:11 -0400

    Add, cross-off and delete functions working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 
+//This adds the checkListItem value to the list as a list item when the addThis button is clicked.
 	$('#addThis').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
         $('#list').append('<li class="items"><button class="listButton">Got it!</button><span>' + toAdd + '</span></li>');
+        //this will clear the input box after the entry has been submitted.
+        $('input[name=checkListItem]').val('')
     });
 
+//This adds the collected class to the list item when clicked.
     $(document).on('click', '.items', function() {
         $(this).toggleClass('collected');
+    });
+
+//This deletes list items withe the collected class when the #delete button is clicked.
+    $('#delete').click(function(){
+    	$('.collected').remove();
     });
 
 
