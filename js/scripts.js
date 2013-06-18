1ef53ff606cef76e5e603a86fb303ff0280b9930
Courtney Wallace <crwallace39@gmail.com>
2013-06-18 15:07:14 -0400

    List/Add function working and Cross off class working

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 
+	$('#addThis').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $('#list').append('<li class="items"><button class="listButton">Got it!</button><span>' + toAdd + '</span></li>');
+    });
+
+    $(document).on('click', '.items', function() {
+        $(this).toggleClass('collected');
+    });
+
+
 	//This will log the #addItems value to the console when the enter key is selected.
-	$('#addItems').keypress(function(e) {
+	/*$('#addItems').keypress(function(e) {
 	    if (e.which == 13) {
-	        console.log($('#addItems').val());
-	        $('#list').append('#items').text($('#addItems').val().add('input'));
-	    }
-	});
+	        var toAdd = $('input[name=checkListItem]').val();
+        	$('#basket').append('<div class="item">' + toAdd + '</div>');
+	});*/
 	
-	//this code marks the grocery items off of the list. 
-	$('.checkbox').click(function(){
-		$(this).next('span').toggleClass('collected');
-	});
 });
